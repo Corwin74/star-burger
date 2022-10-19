125edc8b2fdee7c85e827e090f4f1a76344d0a00
Alexander Samokhin <samokhin@shockland.ru>
2022-10-19 18:43:33 +0300

    Update deployemnt script

--- scripts/deploy_star_burger.sh
+++ scripts/deploy_star_burger.sh
@@ -4,16 +4,16 @@ set -e
 
 cd star-burger/
 source ~/star-burger.env/bin/activate
-git pull
+git pull -q
 pip install -q -r requirements.txt
-/home/burger/.nvm/versions/node/v16.17.1/bin/npm ci --include=dev
-./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
-python manage.py collectstatic --noinput
-python manage.py migrate
+/home/burger/.nvm/versions/node/v16.17.1/bin/npm ci --include=dev > /dev/null 2>&1 
+./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./" > /dev/null 2>&1
+python manage.py collectstatic --noinput -v 0
+python manage.py migrate --noinput -v 0
 sudo /usr/bin/systemctl restart postgresql.service
 sudo /usr/bin/systemctl restart gunicorn.socket
 sudo /usr/bin/systemctl reload nginx.service
-https -q POST https://api.rollbar.com/api/1/deploy X-Rollbar-Access-Token:POST_SERVER_ACCESS_TOKEN \
+https -q POST https://api.rollbar.com/api/1/deploy X-Rollbar-Access-Token:70cf93059b834d44b9f188a50b81e42c \
                         Content-Type:application/json environment=ruvds revision=$(git rev-parse HEAD) \
                         local_username=burger status=succeeded
 echo Deploy complete!
